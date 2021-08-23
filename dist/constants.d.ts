f5208d33509e98f5030acc98f8c895cccb02b8b2
AlbinDalebySnow <albin.daleby@snowsoftware.com>
2021-08-23 11:32:22 +0200

    changed package.json prebuild to use windows remove script instead of mac.
    removed /dist from .gitignore
    
    ran build script

---
+++ dist/constants.d.ts
@@ -0,0 +1,17 @@
+import { ValidationMode } from './types';
+export declare const EVENTS: {
+    BLUR: string;
+    CHANGE: string;
+};
+export declare const VALIDATION_MODE: ValidationMode;
+export declare const INPUT_VALIDATION_RULES: {
+    max: string;
+    min: string;
+    maxLength: string;
+    minLength: string;
+    maxDate: string;
+    minDate: string;
+    pattern: string;
+    required: string;
+    validate: string;
+};
